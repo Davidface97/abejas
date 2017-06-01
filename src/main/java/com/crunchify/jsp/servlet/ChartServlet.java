84ce8d17fc9e65797bd368bfad5047ba2aecf9ff
Labing <Labing@XeonB101-PC-21.academia.loc>
2017-06-01 13:52:03 -0500

    Doble Chart

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -1,8 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.crunchify.jsp.servlet;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Paint;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -11,34 +15,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.labels.ItemLabelAnchor;
-import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.ui.TextAnchor;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
 public class ChartServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("image/png");
-        OutputStream outputStream = response.getOutputStream();
-        JFreeChart chart = getChart();
-        JFreeChart chart2 = getChart2();
-        int width = 500;
-        int height = 350;
-        ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
-        ChartUtilities.writeChartAsPNG(outputStream, chart2, width, height);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        	response.setContentType("image/png");
+		OutputStream outputStream = response.getOutputStream();
+		JFreeChart chart = getChart();
+		int width = 500;
+		int height = 350;
+		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
 
-    }
+	}
 
     public JFreeChart getChart() {
 
