48cddca8882cce01a64db6ed1b1f0a688de3b4b4
Labing <Labing@XeonB101-PC-21.academia.loc>
2017-06-01 13:49:48 -0500

    Doble Chart

--- src/main/java/com/crunchify/jsp/servlet/BarServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/BarServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.crunchify.jsp.servlet;
 
 
@@ -51,7 +46,7 @@ public class BarServlet extends HttpServlet {
         dataset.addValue(5,  "2",  "142"); 
         
  JFreeChart chart = ChartFactory.createBarChart(
-            "Bar Chart Demo 3",       // chart title
+            "Total Kilos Por Colmena",       // chart title
             "Category",               // domain axis label
             "Value",                  // range axis label
             dataset,                  // data
