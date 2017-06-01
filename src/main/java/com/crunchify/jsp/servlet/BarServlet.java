8296a256dcc2e3c3b65cb35d95a4e8f644a5d1ba
USUARIO <USUARIO@PIPE>
2017-06-01 17:37:47 -0500

    Charts

--- src/main/java/com/crunchify/jsp/servlet/BarServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/BarServlet.java
@@ -1,6 +1,5 @@
 package com.crunchify.jsp.servlet;
 
-
 import java.awt.Color;
 import java.awt.Paint;
 import java.io.IOException;
@@ -25,65 +24,34 @@ import org.jfree.ui.TextAnchor;
 
 public class BarServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        	response.setContentType("image/png");
-		OutputStream outputStream = response.getOutputStream();
-		JFreeChart chart = getChart();
-		int width = 500;
-		int height = 350;
-		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("image/png");
+        OutputStream outputStream = response.getOutputStream();
+        JFreeChart chart = getChart();
+        int width = 500;
+        int height = 350;
+        ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
 
-	}
+    }
 
-	public JFreeChart getChart() {
-		
+    public JFreeChart getChart() {
+
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        //Crear la capa de servicios que se enlace con el DAO
+        dataset.setValue("Ford", 23.3);
+        dataset.setValue("Chevy", 32.4);
+        dataset.setValue("Yugo", 44.2);
         
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(15, "1", "451");
-        dataset.addValue(12, "1", "851");
-        dataset.addValue(10, "2", "362");
-        dataset.addValue(5,  "2",  "142"); 
+        boolean textoinferior = true;
+        boolean tool = true;
+        boolean urls = false;
         
- JFreeChart chart = ChartFactory.createBarChart(
-            "Bar Chart Demo 3",       // chart title
-            "Category",               // domain axis label
-            "Value",                  // range axis label
-            dataset,                  // data
-            PlotOrientation.VERTICAL, // the plot orientation
-            false,                    // include legend
-            true,
-            false
-        );
-
-        chart.setBackgroundPaint(Color.lightGray);
-
-        // get a reference to the plot for further customisation...
-        CategoryPlot plot = chart.getCategoryPlot();
-        plot.setNoDataMessage("NO DATA!");
-
-        CategoryItemRenderer renderer = new CustomRenderer(
-            new Paint[] {Color.red, Color.blue, Color.green,
-                Color.yellow, Color.orange, Color.cyan,
-                Color.magenta, Color.blue}
-        );
-
-        renderer.setItemLabelsVisible(true);
-        ItemLabelPosition p = new ItemLabelPosition(
-            ItemLabelAnchor.CENTER, TextAnchor.CENTER, TextAnchor.CENTER, 45.0
-        );
-        renderer.setPositiveItemLabelPosition(p);
-        plot.setRenderer(renderer);
-
-        // change the margin at the top of the range axis...
-        ValueAxis rangeAxis = plot.getRangeAxis();
-        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-        rangeAxis.setLowerMargin(0.15);
-        rangeAxis.setUpperMargin(0.15);
-
+        JFreeChart chart = ChartFactory.createPieChart("Kilos", dataset, textoinferior, tool, urls);
+        
+        
         return chart;
-		
-	}
+    }
 
-}   
+}
